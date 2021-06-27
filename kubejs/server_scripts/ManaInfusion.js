537e5045662a5d9add317618bea72e75e0a96753
= <=>
2021-06-27 17:25:20 -0400

    Initial commit for Splashing & ManaInfusion recipe scripts

---
+++ kubejs/server_scripts/ManaInfusion.js
@@ -0,0 +1,11 @@
+
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+console.info('Changing Mana Infusion Recipes!')
+
+onEvent('recipes', event => {
+
+})
---
+++ kubejs/server_scripts/Splashing.js
@@ -0,0 +1,11 @@
+
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+console.info('Changing Splashing Recipes!')
+
+onEvent('recipes', event => {
+	
+})
