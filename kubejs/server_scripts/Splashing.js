20bb7a1db67fbbfe8b6082ebfe6528c891b3c798
Fart-God <rileypopp@gmail.com>
2021-07-02 08:28:23 -0400

    Remove Create Ore Washing

--- kubejs/server_scripts/Splashing.js
+++ kubejs/server_scripts/Splashing.js
@@ -8,4 +8,9 @@ console.info('Changing Splashing Recipes!')
 
 onEvent('recipes', event => {
 	
+    event.remove({output: '#forge:nuggets', type: 'create:splashing'}) //remove splashing recipes that output nuggets
+    //this removes the gravel>iron, red sand>gold, and soul sand> quartz/gold
+    //this also removes a stage of create ore processing in favor of chaining it with immersive for a greater ore to ingot ratio
+
+
 })
